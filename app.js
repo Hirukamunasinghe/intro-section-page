92efdbc059115603b7e6953933bd2e03abcfe064
Hirukamunasinghe <munasinghehiruka@gmail.com>
2023-12-21 16:15:09 +0530

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// accessing the toggle button
+const toggleButton = document.querySelector('.toggle-button');
+const navBarLinks = document.querySelectorAll('.nav-li');
+const navButtons = document.querySelector('.nav-buttons');
+const heading = document.querySelector('.nav-heading');
+const dropButtonLinks = document.querySelectorAll('.dropbtn')
+
+toggleButton.addEventListener('click', () => {
+    navBarLinks.forEach(link => {
+        link.classList.toggle('active');
+    });
+    dropButtonLinks.forEach(link => {
+        link.classList.toggle('active');
+    });
+    navButtons.classList.toggle('active');
+    heading.classList.add('heading-center');
+});
